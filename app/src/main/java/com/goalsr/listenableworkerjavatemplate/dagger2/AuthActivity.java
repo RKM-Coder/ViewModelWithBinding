06086c65f017b28cf6634c8541887da245daeacd
ram krishna manna <ramkrishna.manna@livsoft.co>
2020-07-09 18:19:25 +0530

    multidaggerdeoendencywithsinglemodule

--- app/src/main/java/com/goalsr/listenableworkerjavatemplate/dagger2/AuthActivity.java
+++ app/src/main/java/com/goalsr/listenableworkerjavatemplate/dagger2/AuthActivity.java
@@ -2,9 +2,13 @@ package com.goalsr.listenableworkerjavatemplate.dagger2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 import com.goalsr.listenableworkerjavatemplate.R;
 
 import javax.inject.Inject;
@@ -12,16 +16,27 @@ import javax.inject.Inject;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity {
-    @Inject
+   /* @Inject
     String toMakeMsg;
     @Inject
-    boolean isApprunning;
+    boolean isApprunning;*/
+
+   @Inject
+    Drawable logoimage;
+   @Inject
+    RequestManager requestManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        setLogo();
+        //Log.e("OUT",toMakeMsg+" Application running"+isApprunning);
+    }
 
-        Log.e("OUT",toMakeMsg+" Application running"+isApprunning);
+    public void setLogo(){
+
+        requestManager.load(logoimage)
+                .into((ImageView)findViewById(R.id.login_logo));
     }
 }
--- app/src/main/java/com/goalsr/listenableworkerjavatemplate/dagger2/di/AppModule.java
+++ app/src/main/java/com/goalsr/listenableworkerjavatemplate/dagger2/di/AppModule.java
@@ -2,13 +2,21 @@ package com.goalsr.listenableworkerjavatemplate.dagger2.di;
 
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.goalsr.listenableworkerjavatemplate.R;
 
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
-    @Provides
+   /* @Provides
     static String saySomething(){
         return "We are ready";
     }
@@ -16,10 +24,28 @@ public class AppModule {
     @Provides
     static boolean getApp(Application application){
         return application==null;
-    }
+    }*/
 
     /*@Provides
     static boolean getStatus(){
         return true;
     }*/
+
+    @Provides
+    static RequestOptions provideRequestOption(){
+        return RequestOptions.placeholderOf(R.mipmap.ic_splash_logo)
+                .error(R.mipmap.mp_3);
+
+    }
+
+    @Provides
+    static RequestManager provideRequestManager(Application application,RequestOptions options){
+        return Glide.with(application)
+                .applyDefaultRequestOptions(options);
+    }
+
+    @Provides
+    static Drawable provideAppDrawable(Application application){
+         return ContextCompat.getDrawable(application,R.mipmap.ic_splash_logo);
+    }
 }
